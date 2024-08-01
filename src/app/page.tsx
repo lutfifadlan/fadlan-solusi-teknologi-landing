80fcf892a3c3c4aa28f24b0643085b26f5ab89dc
lutfifadlan <mochamadlutfifadlan@gmail.com>
2024-08-01 17:44:39 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,20 @@ export default function Home() {
         <meta name="description" content="PT Fadlan Solusi Teknologi - Building technology solutions for the future." />
       </Head>
 
-      <header className="bg-gray-800 text-white py-6 shadow-md">
+      <header className="bg-gray-800 text-white py-4 shadow-md">
         <div className="container mx-auto flex justify-center items-center px-6">
           <h1 className="text-3xl font-bold">PT Fadlan Solusi Teknologi</h1>
         </div>
       </header>
 
-      <main className="container mx-auto p-6">
+      <main className="container mx-auto p-3">
+        <section className="my-3 text-center">
+          <h2 className="text-3xl font-bold text-white">About the Company</h2>
+          <p className="mt-4 text-lg text-gray-300">
+            Fadlan Solusi Teknologi is a company that mainly focuses on solving problems through software technology. One of the primary solutions we offer is Software as a Service (SaaS), providing software solutions to help people solve real-life problems
+          </p>
+        </section>
+
         <section className="my-3 text-center">
           <h2 className="text-3xl font-bold text-white">About the Founder</h2>
           <p className="mt-4 text-lg text-gray-300">
@@ -23,19 +30,19 @@ export default function Home() {
         </section>
 
         <section className="my-12 text-center bg-gray-800 shadow-lg rounded-lg p-6">
-          <h2 className="text-3xl font-bold text-white">Latest Application</h2>
+          <h2 className="text-3xl font-bold text-white">Launched SaaS</h2>
           <div className="mt-4">
             <a href="https://goldofolio.vercel.app/" target="_blank" className="hover:underline text-xl">
               Goldofolio - An Antam Gold Portfolio Tracker
             </a>
             <p className="mt-2 text-lg text-gray-300">
-              Goldofolio helps users track their physical gold portfolios effortlessly. It offers an intuitive dashboard to visualize and manage their gold investments.
+              Goldofolio helps users track their physical gold portfolios effortlessly. It offers an intuitive dashboard to manage their gold investments.
             </p>
           </div>
         </section>
 
         <section className="my-12 text-center">
-          <h2 className="text-3xl font-bold text-white">Experience</h2>
+          <h2 className="text-3xl font-bold text-white">Founder&apos;s Experience</h2>
           <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="bg-gray-800 p-6 shadow-md rounded-md">
               <h3 className="text-xl font-semibold text-white">Engineering Manager - Vida</h3>
@@ -64,9 +71,7 @@ millions dollars transactions volume. Achieved all time high TPV. Contributed to
           <h2 className="text-3xl font-bold text-white">Contact</h2>
           <div className="mt-4">
             <p className="text-lg text-gray-300">Mochamad Lutfi Fadlan</p>
-            <p className="text-lg text-gray-300">+6282117494258</p>
             <p className="text-lg text-gray-300">mochamadlutfifadlan@gmail.com</p>
-            <p className="text-lg text-gray-300">Bavaria Residence 10/8, Babakan, Tangerang, Banten, Indonesia</p>
             <p className="text-lg">
               <a href="https://www.linkedin.com/in/lutfifadlan/" target="_blank" className="hover:underline">
                 LinkedIn Profile
