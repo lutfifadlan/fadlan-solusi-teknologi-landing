5a1d5c52e9c28cfa2fcb733f3993ef75d0ed9662
lutfifadlan <mochamadlutfifadlan@gmail.com>
2025-06-16 22:31:28 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -403,6 +403,12 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {[
               {
+                title: "SyncPoint",
+                description: "Intelligent and reliable appointment management platform that ease collaboration and prevents schedule conflicts",
+                image: "/syncpoint.png",
+                url: "https://appointment-ui.lutfifadlan.com"
+              },
+              {
                 title: "Joblet",
                 description: "Simple job board platform for job seekers and employers.",
                 image: "/joblet.png",
